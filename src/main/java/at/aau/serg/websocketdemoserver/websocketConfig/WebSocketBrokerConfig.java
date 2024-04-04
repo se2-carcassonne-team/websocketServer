f30ec03d2abdaaff5bd2c591490f7fa3f91e3def
Dominik Hartl <gitlab.punctual651@passinbox.com>
2024-04-04 10:37:24 +0200

    refactor: Added /queue prefix as broker topic and added comments for simpler understanding

--- src/main/java/at/aau/serg/websocketdemoserver/websocketConfig/WebSocketBrokerConfig.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocketConfig/WebSocketBrokerConfig.java
@@ -1,10 +1,18 @@
 package at.aau.serg.websocketdemoserver.websocketConfig;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -12,12 +20,16 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        // Messages whose destination begins with /app are routed to the @MessageMapping methods in @Controller classes
         config.setApplicationDestinationPrefixes("/app");
+
+        // Broker topics begin with /topic or /queue
+        config.enableSimpleBroker("/topic", "/queue");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // HTTP URL for the endpoint
         registry.addEndpoint("/websocket-broker")
                 .setAllowedOrigins("*");
     }
