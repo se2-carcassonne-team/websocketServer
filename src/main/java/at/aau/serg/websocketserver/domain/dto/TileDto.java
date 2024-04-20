70fc748196164d5cf0fb98be03bf7b6ee9273099
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-04-20 16:02:05 +0200

    feat: create a tileDto

--- src/main/java/at/aau/serg/websocketserver/domain/dto/TileDto.java
+++ src/main/java/at/aau/serg/websocketserver/domain/dto/TileDto.java
@@ -1,6 +1,5 @@
-package at.aau.serg.websocketserver.domain.entity;
+package at.aau.serg.websocketserver.domain.dto;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,21 +9,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity
-@Table(name = "tile")
-public class TileEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tile_id_seq")
+public class TileDto {
+
     private Long id;
 
     private String tileName;
+
     private String northEdgeType;
     private String southEdgeType;
     private String eastEdgeType;
     private String westEdgeType;
 
-//    Rotation is defined from 0 to 4 (0 north points to north)
     private int rotation;
-
-    //TODO: maybe need to add more attributes
 }
