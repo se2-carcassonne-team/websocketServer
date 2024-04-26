95af38388c2eaaaf40207dd756cc6617ae654d9e
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-04-26 14:41:14 +0200

    feat: Create NextTurnDto object, for the future use

---
+++ src/main/java/at/aau/serg/websocketserver/domain/dto/NextTurnDto.java
@@ -0,0 +1,16 @@
+package at.aau.serg.websocketserver.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NextTurnDto {
+    Long playerId;
+
+    Long tileId;
+}
